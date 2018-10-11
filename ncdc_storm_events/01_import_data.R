c9e3c3b65f85b5582da8d69c209edfd38f5beb1c
Tim Trice <tim.trice@shell.com>
2018-10-11 09:58:31 -0500

    Clean and document script; use csv over rda
    Former-commit-id: 0ab7ddda0fd9405be4f71f1e3e3e29f46d63d08d [formerly 31b216efde2eede8fecf73d1731a90a6d648e8fb]
    Former-commit-id: ddd8c42d706d6b4281b8496b0eed766db2b5cb99

---
+++ ncdc_storm_events/01_import_data.R
@@ -0,0 +1,86 @@
+#' @title ncdc_storm_events
+#' @author Tim Trice <tim.trice@gmail.com>
+#' @description Gather and clean csv.gz datasets from the NCDC Storm Events
+#'   Database. Build three tables; details, fatalities and locations
+#' @source https://www.ncdc.noaa.gov/stormevents/
+
+## Libraries ----
+library(curl)
+library(glue)
+library(janitor)
+library(tidyverse)
+library(XML)
+
+## Options ----
+#' NA
+
+## Settings ----
+#' FTP URL
+ftp <- "ftp://ftp.ncdc.noaa.gov/pub/data/swdi/stormevents/csvfiles/"
+#' Three datasets we'll be obtaining
+tables <- c("details", "fatalities", "locations")
+#' Expected col_types per dataset.
+table_col_types <- list(
+  "details" = glue_collapse(rep("c", 51L)),
+  "fatalities" = glue_collapse(rep("c", 11L)),
+  "locations" = glue_collapse(rep("c", 11L))
+)
+
+## Functions ----
+#' NA
+
+## Import Data ----
+
+## * Connect, download, and load into one dataframe per class
+#' Establish connection, get list of gz datasets
+con = curl(ftp, "r")
+tbl = read.table(con, stringsAsFactors = TRUE, fill = TRUE)
+close(con)
+
+#' Split out the datasets into their own lists. Will end up with a list of
+#' length 3 for each table containing all related dataset URLs
+by_table <-
+  map(
+    .x = glue("^StormEvents_{tables}"),
+    .f = grep,
+    x = tbl$V9,
+    value = TRUE
+  ) %>%
+  set_names(nm = tables)
+
+#' Get the details dataset (this can take a while)
+details <-
+  map_df(
+    .x = glue("{ftp}{by_table$details}"),
+    .f = read_csv,
+    #' Make character to avoid reading errors or warnings
+    col_types = table_col_types$details,
+    progress = TRUE
+  )
+
+save(details, file = "./ncdc_storm_events/details.rda")
+
+#' ...fatalities (can take a while, too)...
+fatalities <-
+  map_df(
+    .x = glue("{ftp}{by_table$fatalities}"),
+    .f = read_csv,
+    #' Make character to avoid reading errors or warnings
+    col_types = table_col_types$fatalities,
+    progress = TRUE
+  )
+
+save(fatalities, file = "./ncdc_storm_events/fatalities.rda")
+
+#' ...and locations (a bit faster.
+locations <-
+  map_df(
+    .x = glue("{ftp}{by_table$locations}"),
+    .f = read_csv,
+    #' Make character to avoid reading errors or warnings
+    col_types = table_col_types$locations,
+    progress = TRUE
+  )
+
+save(locations, file = "./ncdc_storm_events/locations.rda")
+
