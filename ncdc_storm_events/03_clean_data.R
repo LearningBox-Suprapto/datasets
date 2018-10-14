924b132b92e514167e3304844f2c6d78dd40e6fd
Tim Trice <tim.trice@gmail.com>
2018-10-14 08:43:27 -0500

    Add libraries, sources, data
    Former-commit-id: 7e10f8418e8ad1f6b2485ae2d2bff884f5772415 [formerly 7fc6dd8f334d128212cb78e195df5c651612107b]
    Former-commit-id: e0eaae88ae03aaa95569063309ae881fe4f1f922

--- ncdc_storm_events/03_clean_data.R
+++ ncdc_storm_events/03_clean_data.R
@@ -1,3 +1,12 @@
 #' @author Tim Trice <tim.trice@gmail.com>
 #' @description Clean NCDC Storm Events Database
 #' @source https://www.ncdc.noaa.gov/stormevents/
+
+# ---- libraries ----
+library(here)
+
+# ---- sources ----
+source(here("./ncdc_storm_events/functions.R"))
+
+# ---- data ----
+load(file = here("./ncdc_storm_events/ncdc_storm_events.RData"))
