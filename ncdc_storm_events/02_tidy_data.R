99f1cadf9025bfe18da04d8b2f8cf2837ba96b88
Tim Trice <tim.trice@gmail.com>
2018-10-14 08:29:32 -0500

    Remove functions to file; source
    Former-commit-id: 380ff2fb6cf0ef40b951d9a2d952f17255d99b0c [formerly cdd47eff597824225d3442f36495c2a743a6b20a]
    Former-commit-id: 96894b9c7c3b2c526230122182d22e3a8afe6604

--- ncdc_storm_events/02_tidy_data.R
+++ ncdc_storm_events/02_tidy_data.R
@@ -8,30 +8,11 @@ library(purrr)
 library(stringr)
 library(tidyr)
 
+# ---- sources ----
+source(here("./ncdc_storm_events/functions.R"))
+
 # ---- data ----
 load(file = here("./ncdc_storm_events/ncdc_storm_events.RData"))
-
-# ---- functions ----
-#' @title var_conversion
-#' @description Test variables in dataframe and convert, without error, to
-#'   integer, double, or leave as character.
-#' @param x Variable to test
-var_conversion <- function(x) {
-
-  quietly_as_integer <- quietly(.f = as.integer)
-  quietly_as_double <- quietly(.f = as.double)
-
-  y <- quietly_as_integer(x)
-  z <- quietly_as_double(x)
-
-  if (all(y$result == z$result) & !is.na(all(y$result == z$result)))
-    return(y$result)
-
-  if (all(is_empty(z$warnings), is_empty(z$messages)))
-    return(z$result)
-
-  return(x)
-}
 
 # ---- convert-columns ----
 df <- map(df, mutate_all, .funs = var_conversion)
